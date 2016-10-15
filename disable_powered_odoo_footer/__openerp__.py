e37d22a3cb4a9f66fb1271f42e6a8d6e76a45e44
mstuttgart <michellstut@gmail.com>
2016-10-14 21:31:48 -0300

    [FIX] Removido comentarios da view

--- disable_powered_odoo_footer/__openerp__.py
+++ disable_powered_odoo_footer/__openerp__.py
@@ -5,7 +5,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': "Disable Powered by Odoo Footer",
+    'name': 'Disable Powered by Odoo Footer',
     'license': 'AGPL-3',
     'author': 'MultidadosTI',
     'version': '8.0.1.0.0',
@@ -17,7 +17,6 @@
     'data': [
         'views/view_disable_powered_odoo_footer.xml',
     ],
-    'demo': [],
     'installable': True,
     'auto_install': True,
 }
