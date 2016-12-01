7e74582291f157494ebc31de3c11aea84f0a18f5
Aldo774 <soares_aldo@hotmail.com>
2016-11-30 23:58:42 -0200

    [FIX] Mudança do tipo de relação em projects para Many2many

--- multi-project/models/multi_project.py
+++ multi-project/models/multi_project.py
@@ -6,23 +6,30 @@ from odoo import models, fields
 class Meeting(models.Model):
     _inherit = "calendar.event"
 
-    project_ids = fields.One2many(comodel_name="project.project",
-                                   inverse_name="project_id",
-                                   string="Projetos")
+    # project_ids = fields.One2many(comodel_name="project.project",
+    #                                inverse_name="project_id",
+    #                                string="Projetos")
 
-    task_ids = fields.One2many(comodel_name="project.task",
-                                   inverse_name="task_id",
-                                   string="Tarefas")
+    project_ids = fields.Many2many("project.project",
+                                   string="Projetos",
+                                   domain=[('partner_id','in', 'attendee_ids')])
 
 
-class Project(models.Model):
-    _inherit = "project.project"
+    def get_list_partners(self):
+        return self.partner_ids
+    # task_ids = fields.One2many(comodel_name="project.task",
+    #                                inverse_name="task_id",
+    #                                string="Tarefas")
 
-    project_id = fields.Many2one(comodel_name="calendar.event",
-                                 string="Projeto")
 
-class Task(models.Model):
-    _inherit = "project.task"
+# class Project(models.Model):
+#     _inherit = "project.project"
+#
+#     project_id = fields.Many2one(comodel_name="calendar.event",
+#                                  string="Projeto")
 
-    task_id = fields.Many2one(comodel_name="calendar.event",
-                                 string="Tarefa")
+# class Task(models.Model):
+#     _inherit = "project.task"
+#
+#     task_id = fields.Many2one(comodel_name="calendar.event",
+#                                  string="Tarefa")
