794fd68b1024c81c9863a762eb97ed8814dac1f4
Aldo_Soares <soares_aldo@hotmail.com>
2016-12-02 14:02:38 -0200

    [FIX] Adicionado domain em fields de projeto e tarefa

--- multi-project/models/multi_project.py
+++ multi-project/models/multi_project.py
@@ -6,30 +6,7 @@ from odoo import models, fields
 class Meeting(models.Model):
     _inherit = "calendar.event"
 
-    # project_ids = fields.One2many(comodel_name="project.project",
-    #                                inverse_name="project_id",
-    #                                string="Projetos")
+    project_id = fields.Many2one("project.project",
+                                 string="Projetos")
 
-    project_ids = fields.Many2many("project.project",
-                                   string="Projetos",
-                                   domain=[('partner_id','in', 'attendee_ids')])
-
-
-    def get_list_partners(self):
-        return self.partner_ids
-    # task_ids = fields.One2many(comodel_name="project.task",
-    #                                inverse_name="task_id",
-    #                                string="Tarefas")
-
-
-# class Project(models.Model):
-#     _inherit = "project.project"
-#
-#     project_id = fields.Many2one(comodel_name="calendar.event",
-#                                  string="Projeto")
-
-# class Task(models.Model):
-#     _inherit = "project.task"
-#
-#     task_id = fields.Many2one(comodel_name="calendar.event",
-#                                  string="Tarefa")
+    task_id = fields.Many2one("project.task", string="Tarefas")
