0da0fb66400a6117fb88740b6e2f2947c9904a2b
Michell Stuttgart <michellstut@gmail.com>
2016-12-05 16:19:23 -0200

    [NEW] Modulo renomeado para project_task_calendar

--- project_task_calendar/models/calendar.py
+++ project_task_calendar/models/calendar.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+# # Project Task Calendar
+# Copyright (C) 2016 MultidadosTI (http://www.multidadosti.com.br)
+# @author Aldo Soares <a.soares@itimpacta.org.br>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import models, fields
 
