f563fc4d6ba4344fa425270b9c049d97f34a058d
Aldo_Soares <soares_aldo@hotmail.com>
2016-12-08 11:08:51 -0200

    [IMP] Fields: Novo field *is_default*
    Em project.tasks, campo atribui o atual estágio automaticamente a todos os novos projetos criados

--- project_task_calendar/models/project.py
+++ project_task_calendar/models/project.py
@@ -10,3 +10,12 @@ class ProjectProject(models.Model):
     calendar_event_ids = fields.One2many(comodel_name="calendar.event",
                                          inverse_name="project_id",
                                          string=u"Eventos de Calendário")
+
+
+class MultiProjectTaskType(models.Model):
+
+    _inherit = 'project.task.type'
+
+    is_default = fields.Boolean(string=u"Padrão",
+                                help=u"Atribui o atual estágio automaticamente"
+                                     u" a todos os novos projetos criados")
