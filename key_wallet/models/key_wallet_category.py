281a433546707c812a835e417b3ad1674e6c6b54
Michell Stuttgart <michellstut@gmail.com>
2016-12-08 11:17:08 -0200

    [NEW] models renomeados

---
+++ key_wallet/models/key_wallet_category.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#    
+#    Copyright (C) 2016 MultidadosTI (http://www.multidadosti.com.br)
+#    @author Michell Stuttgart <m.faria@itimpacta.org.br>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models, fields
+
+
+class ResPasswordCategory(models.Model):
+
+    _name = 'key.wallet.category'
+    _description = 'Password Category'
+
+    name = fields.Char(string='Name', required=True)
+    res_password_ids = fields.Many2many('key.wallet.password',
+                                        string='Passwords',
+                                        readonly=True)
--- key_wallet/models/key_wallet_password.py
+++ key_wallet/models/key_wallet_password.py
@@ -4,22 +4,24 @@
 #    @author Michell Stuttgart <m.faria@itimpacta.org.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import api, fields, models
 
 
 class ResPassword(models.Model):
 
-    _name = 'res.password'
+    _name = 'key.wallet.password'
     _rec_name = 'title'
     _description = 'Represents password key'
 
-    title = fields.Char(string='Title')
+    title = fields.Char(string='Title', required=True)
     url = fields.Char(string='URL')
     username = fields.Char(string='Username')
     password = fields.Char(string='Password')
+    password_visible = fields.Char(string='Password', related='password')
+    show_password = fields.Boolean(string='Show Password', default=False)
     description = fields.Html(string='Description')
     group_id = fields.Many2one('res.groups', string='Owner Group')
-    category_id = fields.Many2one('res.password.category', string='Category')
+    category_ids = fields.Many2many('key.wallet.category', string='Category')
 
     create_date = fields.Datetime(index=True, string='Date', readonly=True)
     write_date = fields.Datetime(index=True, string='Date', readonly=True)
@@ -28,12 +30,18 @@ class ResPassword(models.Model):
                                  default=lambda self: self.env.uid)
     write_uid = fields.Many2one('res.users', string='By', readonly=True)
 
+    @api.model
+    def create(self, values):
+        # values['show_password'] = False
+        values.update({'show_password': False})
+        return super(ResPassword, self).create(vals=values)
 
-class ResPasswordCategory(models.Model):
+    @api.multi
+    def write(self, values):
+        values.update({'show_password': False})
+        # values['show_password'] = False
+        return super(ResPassword, self).write(vals=values)
 
-    _name = 'res.password.category'
-    _description = 'Password Category'
-
-    name = fields.Char(string='Name', required=True)
-    res_password_ids = fields.One2many('res.password', 'category_id',
-                                       string='Passwords')
+    @api.onchange('password_visible')
+    def onchange_password_visible(self):
+        self.password = self.password_visible
