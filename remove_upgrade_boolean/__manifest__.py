879c4b057407202c2ba085f22b5160d25a37ace0
Aldo_Soares <soares_aldo@hotmail.com>
2016-12-12 11:47:26 -0200

    [IMP] Refatoramento do módulo para otimização
    Renomeação do módulo, bem como exclusão de algumas dependências

--- remove_upgrade_boolean/__manifest__.py
+++ remove_upgrade_boolean/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-#    Remove boolean upgrade
+#    Remove upgrade boolean
 #    Copyright (C) 2016 MultidadosTI (http://www.multidadosti.com.br)
 #    @author Aldo Soares <a.soares@itimpacta.org.br>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Remove boolean upgrade',
+    'name': 'Remove upgrade boolean',
     'license': 'AGPL-3',
     'author': 'MultidadosTI',
     'version': '10.0.1.0.0',
@@ -16,34 +16,21 @@
 This module refactor these base modules:
 ========================================
 
-* mass_mailing (Mass Mailing Campaigns)
-* stock (Inventory Management)
 * base_setup (Initial Setup Tools)
 * sale (Sales)
 * account (Invoicing)
-* website_sale (eCommerce)
-* project (Project)
-* point_of_sale (Point of Sale)
-* crm (CRM)
 
-Remove all fields with widget *boolean_upgrade*.
+Remove all fields with widget *upgrade_boolean*.
                        """,
     'category': 'Web',
     'sequence': 99,
     'depends': [
-        'mass_mailing',
-        'stock',
         'base_setup',
         'sale',
         'account',
-        'website_sale',
-        'project',
-        'point_of_sale',
-        'crm',
-
     ],
     'data': [
-        'views/view_remove_boolean_upgrade.xml',
+        'views/view_remove_upgrade_boolean.xml',
     ],
     'installable': True,
 }
