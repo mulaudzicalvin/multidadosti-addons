53fd5fec36e0bbb85b4f8751f8cd8f204eba3be7
Michell Stuttgart <michellstut@gmail.com>
2016-12-12 14:13:48 -0200

    [FIX] Ajuste copyright

--- project_task_calendar/models/project.py
+++ project_task_calendar/models/project.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+# Project Task Calendar
+# Copyright (C) 2016 MultidadosTI (http://www.multidadosti.com.br)
+# @author Michell Stuttgart <michellstut@gmail.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 import pytz
 
