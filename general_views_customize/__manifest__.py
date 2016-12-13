7c9e6ee4974888edcf2d169646c78cadba3b662f
Aldo_Soares <soares_aldo@hotmail.com>
2016-12-13 14:46:34 -0200

    [IMP] Adicionado icone customizado em abas do sistema

--- general_views_customize/__manifest__.py
+++ general_views_customize/__manifest__.py
@@ -5,7 +5,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'General Views Customize to Odoo',
+    'name': 'General Views Customize',
     'license': 'AGPL-3',
     'author': 'MultidadosTI',
     'version': '10.0.1.0.0',
