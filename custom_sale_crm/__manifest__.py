46fa6faf36bad3c7b691ad497184308ae94d5f71
Aldo_Soares <soares_aldo@hotmail.com>
2016-12-15 15:22:50 -0200

    [FIX] Renomeação do módulo

--- custom_sale_crm/__manifest__.py
+++ custom_sale_crm/__manifest__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-#    Custom CRM Leads
+#    Custom Sale CRM
 #    Copyright (C) 2016 MultidadosTI (http://www.multidadosti.com.br)
 #    @author Aldo Soares <soares_aldo@hotmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Custom CRM Leads',
+    'name': 'Custom Sale CRM',
     'license': 'AGPL-3',
     'author': 'MultidadosTI',
     'version': '10.0.1.0.0',
     'website': 'https://github.com/multidadosti-erp/multidadosti-addons',
-    'summary': 'Custom CRM Leads',
+    'summary': 'Custom Sale CRM',
     'category': 'Web',
     'sequence': 99,
     'depends': [
@@ -19,7 +19,7 @@
         'sale_crm',
     ],
     'data': [
-        'views/view_custom_crm_leads.xml',
+        'views/view_custom_sale_crm.xml',
     ],
     'installable': True,
 }
