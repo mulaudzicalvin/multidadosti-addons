2f8a56012f4c769ce0b852328ea63065e2239c29
Michell Stuttgart <michellstut@gmail.com>
2016-12-16 11:28:57 -0200

    [FIX] Erros ao criar tabela

--- general_calendar/models/general_calendar.py
+++ general_calendar/models/general_calendar.py
@@ -4,7 +4,7 @@
 # @author Michell Stuttgart <michellstut@gmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import api, fields, models, tools
 
 
 def _models_get(self):
@@ -16,6 +16,7 @@ def _models_get(self):
 class GeneralCalendar(models.Model):
 
     _name = 'general.calendar'
+    _auto = False
 
     name = fields.Char(
         string='Description',
@@ -51,3 +52,36 @@ class GeneralCalendar(models.Model):
         string='Model',
         readonly=True,
     )
+
+    @api.model_cr
+    def init(self):
+        tools.drop_view_if_exists(self.env.cr, self._table)
+        #
+        # lines = self.env['general.calendar.line'].search([])
+        #
+        # sql = ''
+        # selects = []
+        #
+        # for line in lines:
+        #     """SELECT
+        #     line.id as id,
+        #     line.name.name as name,
+        #     line.date_start_field_id as date_start,
+        #     line.duration_field_id as duration,
+        #     line.user_field_id as user_id
+        # FROM
+        # general_calendar_line
+        # """
+
+        sql = """CREATE VIEW %s AS
+                SELECT   ce.id as id,
+                         ce.name as name,
+                         ce.start as date_start,
+                         ce.duration as duration,
+                         ce.user_id as user_id,
+                         NULL as model_id
+                FROM calendar_event ce
+                WHERE ce.active = 'true'
+                """ % self._table
+
+        self.env.cr.execute(sql)
