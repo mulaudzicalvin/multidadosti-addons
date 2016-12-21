bb7da0f6e67da5d77d48113a659fc7fdb0086192
Michell Stuttgart <michellstut@gmail.com>
2016-12-21 15:23:18 -0200

    [FIX] Removido description

--- custom_user_hr_signature/__manifest__.py
+++ custom_user_hr_signature/__manifest__.py
@@ -11,9 +11,6 @@
     'version': '10.0.1.0.0',
     'website': 'https://github.com/multidadosti-erp/multidadosti-addons',
     'summary': 'Custom User HR Signature',
-    'description': """
-        This module adds a signature field in hr module
-    """,
     'category': 'Web',
     'contributors': [
         'Aldo Soares <soares_aldo@hotmail.com>',
