1405415e9bc258c1e9f061ed18bc9011e803d7df
Michell Stuttgart <michellstut@gmail.com>
2016-12-21 13:21:19 -0200

    [FIX] Ajustes copyright

--- custom_user_hr_signature/models/custom_user_hr_signature.py
+++ custom_user_hr_signature/models/custom_user_hr_signature.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
+# Custom User HR Signature
+# Copyright (C) 2016 MultidadosTI (http://www.multidadosti.com.br)
+# @author Aldo Soares <soares_aldo@hotmail.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 from odoo import models, fields
 
 
 class CustomUserHRSignature(models.Model):
+
     _inherit = 'hr.employee'
 
-    signature = fields.Binary("Assinatura",
+    signature = fields.Binary("Signature",
                               attachment=True,
                               help="This field holds the image used as photo "
                                    "for the employee, limited to 1024x1024px.")
