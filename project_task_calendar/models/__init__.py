1a32aa2ff32dce5f8c0ab6cdb4218a0bcf982e2f
Michell Stuttgart <michellstut@gmail.com>
2016-12-22 10:33:07 -0200

    [FIX] Ajustes copyright

--- project_task_calendar/models/__init__.py
+++ project_task_calendar/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Multidados Project
 # Copyright (C) 2016 MultidadosTI (http://www.multidadosti.com.br)
-# @author Aldo Soares <a.soares@itimpacta.org.br>
+# @author Aldo Soares <soares_aldo@hotmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import calendar
