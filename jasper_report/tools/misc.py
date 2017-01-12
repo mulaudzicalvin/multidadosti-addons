9f301951f622ccd5df24904cfff5631c6e49e0a6
Aldo_Soares <soares_aldo@hotmail.com>
2017-01-12 18:17:23 -0200

    [IMP] Implementação de backup para relatórios jasper

---
+++ jasper_report/tools/misc.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 MultidadosTI (http://www.multidadosti.com.br)
+# @author Aldo Soares <soares_aldo@hotmail.com>
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+import os
+
+from odoo.tools import config
+from odoo.tools.translate import _
+from odoo.exceptions import RedirectWarning
+
+
+def mount_path_jasper(dbname, id_report, report='jasper'):
+    db_name = dbname
+    filestore = config.filestore(db_name)
+    report_path = '/'.join([filestore,
+                            'jasper_report',
+                            str(report) + "_" + str(id_report)])
+
+    if not os.path.exists(report_path):
+        try:
+            os.makedirs(report_path)
+        except OSError:
+            raise RedirectWarning(
+                _(u'Erro!'),
+                _(u"""Verifique as permissões de escrita
+                e o caminho da pasta"""))
+    return report_path
