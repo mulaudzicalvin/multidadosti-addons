9346d72b4ee17f56cf15c97f91598c6202465522
Michell Stuttgart <michellstut@gmail.com>
2017-02-08 10:29:07 -0200

    [DEL] Jasper report addon

--- jasper_report/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2017 MultidadosTI (http://www.multidadosti.com.br)
-# @author Aldo Soares <soares_aldo@hotmail.com>
-# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
-
-from . import jasper_report_settings
-from . import ir_action_report_xml
