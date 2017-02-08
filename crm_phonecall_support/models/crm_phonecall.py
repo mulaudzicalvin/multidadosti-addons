0bb96a54419e3143557ff2f9b02c860455a841a8
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2017-02-08 18:27:09 -0200

    Ajuste no código do módulo

--- crm_phonecall_support/models/crm_phonecall.py
+++ crm_phonecall_support/models/crm_phonecall.py
@@ -1,13 +1,31 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2017 MultidadosTI (http://www.multidadosti.com.br)
-# @author Michell Stuttgart <m.faria@itimpacta.org.br>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# Copyright (C) 2016 MultidadosTI (http://www.multidadosti.com.br)
+# @author Rodrigo Ferreira <rodrigosferreira91@gmail.com>
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
 from odoo import api, fields, models
 
 
-class NewModule(models.Model):
-    _name = 'new_module.new_module'
-    _description = 'New Description'
+class PhoneCallCrm(models.Model):
 
-    name = fields.Char()
+    _name = 'phonecall.service'
+
+    name = fields.Text(string='Description')
+
+    start_date_hour = fields.Datetime(string='Start Date')
+
+    partner_id = fields.Many2one(string='Partner',
+                              comodel_name='res.partner')
+
+    project_id = fields.Many2one(comodel_name='project.project')
+
+    contact_partner_id = fields.Many2one(string='Contact',
+                              comodel_name='res.partner')
+
+    user_id = fields.Many2one(string='User',
+                              comodel_name='res.users')
+
+    finish_date_hour = fields.Datetime(string='Finish Date')
+
+
+
