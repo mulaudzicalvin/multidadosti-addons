88293da11deef32c7cdaa383bebbb537729cd9bb
Michell Stuttgart <michellstut@gmail.com>
2017-02-14 18:28:47 -0200

    [DEL] Modulo 'project_timesheet' removido

--- project_timesheet/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2017 MultidadosTI (http://www.multidadosti.com.br)
-# @author Michell Stuttgart <michellstut@gmail.com>
-# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
-
-from . import account_analytic_line
-from . import to_invoice
