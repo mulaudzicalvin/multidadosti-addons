eb26204440ae94440b74b391c4d0926c45dbc9b9
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2017-02-14 18:26:08 -0200

    [NEW]Adicionei form wizar.calendar para o button done

--- project_task_calendar/models/calendar.py
+++ project_task_calendar/models/calendar.py
@@ -3,7 +3,7 @@
 # @author Aldo Soares <soares_aldo@hotmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class CalendarEvent(models.Model):
@@ -17,5 +17,41 @@ class CalendarEvent(models.Model):
         ('done', 'Done'),
         ('cancel', 'Cancel')
     ], string='State', default='open')
-    time = fields.Float(string="Time")
+    meeting_feedback = fields.Text(string='Feedback', readonly=True)
+    meeting_duration = fields.Float(string='Duration', readonly=True)
 
+    @api.multi
+    def done_button(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'wizard.calendar.event',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'views': [(False, "form")],
+            'target': 'new',
+            'context': {
+                'default_calendar_event_id': self.id,
+                'default_meeting_duration': self.duration,
+            }
+            # 'res_id': self.id,
+        }
+
+    def cancel_button(self):
+        self.meeting_state = 'cancel'
+
+    def open_button(self):
+        self.meeting_state = 'open'
+
+
+class WizardCalendarEventDone(models.TransientModel):
+    _name = 'wizard.calendar.event'
+
+    meeting_feedback = fields.Text(string='Feedback', required=True)
+    meeting_duration = fields.Float(string='Duration', required=True)
+    calendar_event_id = fields.Many2one('calendar.event')
+
+    def finish(self):
+        self.calendar_event_id.meeting_state = 'done'
+        self.calendar_event_id.meeting_feedback = self.meeting_feedback
+        self.calendar_event_id.meeting_duration = self.meeting_duration
