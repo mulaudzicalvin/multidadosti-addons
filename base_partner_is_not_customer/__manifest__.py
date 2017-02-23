68e996f382ef9dbc6206fdb106cf254d3f0d52e1
Michell Stuttgart <michellstut@gmail.com>
2017-02-23 09:42:23 -0300

    [NEW] Adicionado modulo 'base_partner_is_not_customer'

---
+++ base_partner_is_not_customer/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Partner Is Not Customer',
+    'version': '10.0.1.0.0',
+    'author': 'MultidadosTi',
+    'maintainer': 'MultidadosTi',
+    'website': 'www.multidadosti.com.br',
+    'license': 'LGPL-3',
+    'category': 'Base',
+    'summary': """This module set default value of \'customer\' field
+    from \'res.partner\' model like False.""",
+    'contributors': [
+        'Michell Stuttgart <michellstut@gmail.com>',
+    ],
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
