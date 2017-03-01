1d8afa6e0874402c23f887746a9ca026a015a254
Aldo_Soares <soares_aldo@hotmail.com>
2017-03-01 11:59:23 -0300

    [IMP] Adicionado domain em Projetos para incluir documentos de calendar.event

--- project_task_calendar/models/project.py
+++ project_task_calendar/models/project.py
@@ -3,12 +3,52 @@
 # @author Michell Stuttgart <michellstut@gmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class ProjectProject(models.Model):
 
     _inherit = 'project.project'
+
+    @api.multi
+    def attachment_tree_view(self):
+        self.ensure_one()
+        domain = [
+            '|',
+            '|',
+            '&', ('res_model', '=', 'project.project'), ('res_id', 'in', self.ids),
+            '&', ('res_model', '=', 'calendar.event'), ('res_id', 'in', self.calendar_event_ids.ids),
+            '&', ('res_model', '=', 'project.task'), ('res_id', 'in', self.task_ids.ids)]
+        return {
+            'name': _('Attachments'),
+            'domain': domain,
+            'res_model': 'ir.attachment',
+            'type': 'ir.actions.act_window',
+            'view_id': False,
+            'view_mode': 'kanban,tree,form',
+            'view_type': 'form',
+            'help': _('''<p class="oe_view_nocontent_create">
+                        Documents are attached to the tasks and issues of your project.</p><p>
+                        Send messages or log internal notes with attachments to link
+                        documents to your project.
+                    </p>'''),
+            'limit': 80,
+            'context': "{'default_res_model': '%s','default_res_id': %d}" % (self._name, self.id)
+        }
+
+    def _compute_attached_docs_count(self):
+        Attachment = self.env['ir.attachment']
+        for project in self:
+            project.doc_count = Attachment.search_count([
+                '|',
+                '|',
+                '&',
+                ('res_model', '=', 'project.project'), ('res_id', '=', project.id),
+                '&',
+                ('res_model', '=', 'calendar.event'), ('res_id', 'in', project.calendar_event_ids.ids),
+                '&',
+                ('res_model', '=', 'project.task'), ('res_id', 'in', project.task_ids.ids)
+            ])
 
     partner_ids = fields.Many2many(comodel_name='res.partner',
                                    string='Related Partners')
