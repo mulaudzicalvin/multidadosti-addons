5472780b49f40f38f5b7ca3fd501ef7433afd29b
Michell Stuttgart <michellstut@gmail.com>
2017-03-02 17:23:01 -0300

    [NEW] Adicionado 'help' ao campo 'locate_partner'

--- base_user_from_partner/models/res_user.py
+++ base_user_from_partner/models/res_user.py
@@ -6,4 +6,6 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    locate_partner = fields.Boolean(string='Select Existing Partner?')
+    locate_partner = fields.Boolean(string='Select Existing Partner?',
+                                    help='Allow select a existing partner '
+                                         'to use as base to create this user.')
