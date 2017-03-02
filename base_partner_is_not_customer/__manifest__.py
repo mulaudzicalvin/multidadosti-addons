ec8525b1c2fee9770c1c6057b96a282367eb0759
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2017-03-02 16:41:15 -0300

    [FIX] Agora Partner criado apartir do usuário não é setado como cliente

--- base_partner_is_not_customer/__manifest__.py
+++ base_partner_is_not_customer/__manifest__.py
@@ -18,7 +18,6 @@
     ],
     'data': [
         'views/res_partner_view.xml',
-        'views/res_users_view.xml',
     ],
     'installable': True,
     'auto_install': True,
--- base_partner_is_not_customer/models/res_users.py
+++ base_partner_is_not_customer/models/res_users.py
@@ -6,15 +6,15 @@ from odoo import api, fields, models
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    locate_partner = fields.Boolean(string='Select Existing Partner?')
-
     @api.model
-    def create(self, vals):
-        user = super(ResUsers, self).create(vals)
-        user.partner_id.active = user.active
-        if user.partner_id.company_id:
-            user.partner_id.write({'company_id': user.company_id.id,
-                                   'customer': False,
-                                   'parent_id': user.company_id.partner_id.id,
-                                   'supplier': False})
+    def create(self, values):
+        user = super(ResUsers, self).create(values)
+        # user.partner_id.active = user.active
+        if not values['partner_id']:
+            user.partner_id.write({
+                'company_id': user.company_id.id,
+                'customer': False,
+                'parent_id': user.company_id.partner_id.id,
+                'supplier': False,
+            })
         return user
