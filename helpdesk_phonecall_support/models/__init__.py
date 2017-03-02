75ae9764b5f9650aa57ea954af74906c2419f93f
Michell Stuttgart <michellstut@gmail.com>
2017-03-02 15:19:23 -0300

    [FIX] Removido copyright do codigo fonte

--- helpdesk_phonecall_support/models/__init__.py
+++ helpdesk_phonecall_support/models/__init__.py
@@ -1,6 +1,3 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016 MultidadosTI (http://www.multidadosti.com.br)
-# @author Rodrigo Ferreira <rodrigosferreira91@gmail.com>
-# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
 from . import helpdesk_phonecall_service
--- helpdesk_phonecall_support/models/helpdesk_phonecall_service.py
+++ helpdesk_phonecall_support/models/helpdesk_phonecall_service.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016 MultidadosTI (http://www.multidadosti.com.br)
-# @author Rodrigo Ferreira <rodrigosferreira91@gmail.com>
-# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
 from odoo import api, fields, models
 
 
 class HelpDeskPhoneCall(models.Model):
+
     _name = 'helpdesk.phonecall.service'
     _rec_name = 'title'
 
