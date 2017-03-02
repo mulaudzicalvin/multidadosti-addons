7d946c7f6a031a7bd540bb4859acc1307e38a3f3
Michell Stuttgart <michellstut@gmail.com>
2017-03-02 15:18:47 -0300

    [NEW] Adicionado 'project_team' como dependencia

--- helpdesk_phonecall_support/__manifest__.py
+++ helpdesk_phonecall_support/__manifest__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016 MultidadosTI (http://www.multidadosti.com.br)
-# @author Rodrigo Ferreira <rodrigosferreira91@gmail.com>
-# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
 {
     'name': 'PhoneCall Support',
@@ -15,7 +12,7 @@
         'Rodrigo Ferreira <rodrigosferreira91@gmail.com>',
     ],
     'depends': [
-        'project',
+        'project_team',
         'hr_timesheet',
     ],
     'data': [
