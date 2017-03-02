c9e62f18938338d15a3dfce1838dcb7a1230d249
Michell Stuttgart <michellstut@gmail.com>
2017-03-02 17:34:38 -0300

    [FIX] Tradução do 'base_user_from_partner'

--- base_user_from_partner/models/res_user.py
+++ base_user_from_partner/models/res_user.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models, _
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
     locate_partner = fields.Boolean(string='Select Existing Partner?',
-                                    help='Allow select a existing partner '
-                                         'to use as base to create this user.')
+                                    help=_("""Allow select a existing
+                                           partner to use as base to create
+                                           this user."""))
