62c1d0f179c193c32f0695991188a0257b59cd87
Michell Stuttgart <michellstut@gmail.com>
2017-03-03 17:52:01 -0300

    [FIX] Removido arquivos de copyright 'block_crm_quotation_for_stage'

--- block_crm_quotation_for_stage/models/sale_crm.py
+++ block_crm_quotation_for_stage/models/sale_crm.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016 MultidadosTI (http://www.multidadosti.com.br)
-# @author Aldo Soares <soares_aldo@hotmail.com>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import fields, models
 
@@ -9,8 +6,7 @@ from odoo import fields, models
 class CRMStage(models.Model):
     _inherit = "crm.stage"
 
-    allow_quotation = fields.Boolean(string='Allow Quotation',
-                                     default=True,
+    allow_quotation = fields.Boolean(default=True,
                                      help='Allow create quotation when '
                                           'opportunity is in this state.')
 
