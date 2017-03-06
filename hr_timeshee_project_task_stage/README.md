c5997eb092a0fe8a75d0a5104b7347cf7f315773
Michell Stuttgart <michellstut@gmail.com>
2017-03-06 16:00:10 -0300

    [NEW] Adicionado modulo 'hr_timeshee_project_task_stage'.

---
+++ hr_timeshee_project_task_stage/README.md
@@ -0,0 +1,14 @@
+## HR Timeshee Project Task Stage
+
+Allow use project task stage to filter stages in timesheet.
+
+### Bug Tracker
+
+Bugs are tracked on [GitHub Issues](https://github.com/multidadosti-erp/multidadosti-addons/issues).
+
+In case of trouble, please check there if your issue has already been reported.
+
+### Contributors
+
+* Aldo Soares <soares_aldo@hotmail.com>
+* Michell Stuttgart <michellstut@gmail.com>
--- hr_timeshee_project_task_stage/__manifest__.py
+++ hr_timeshee_project_task_stage/__manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'HR Timesheet to Invoice',
+    'name': 'HR Timeshee Project Task Stage',
     'version': '10.0.1.0.0',
     'author': 'MultidadosTI',
     'maintainer': 'MultidadosTI',
     'website': 'www.multidadosti.com.br',
     'license': 'LGPL-3',
     'category': 'Timesheets',
-    'summary': 'Allow check a timesheet record will be invoiced or not',
+    'summary': 'Use project task stage to filter stages in timesheet',
     'contributors': [
         'Aldo Soares <soares_aldo@hotmail.com>',
         'Michell Stuttgart <michellstut@gmail.com>',
@@ -17,10 +17,7 @@
         'hr_timesheet',
     ],
     'data': [
-        'security/ir.model.access.csv',
-        'data/account_analytic_line_to_invoice.xml',
-        'views/account_analytic_line_view.xml',
-        'views/account_analytic_line_to_invoice.xml'
+        'views/account_analytic_line.xml',
     ],
     'installable': True,
     'auto_install': False,
