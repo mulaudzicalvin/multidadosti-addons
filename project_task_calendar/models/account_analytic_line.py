6fd05e44c30b05e1db07dde0f92038ea9a31f42d
Michell Stuttgart <michellstut@gmail.com>
2017-03-06 16:00:10 -0300

    [NEW] Adicionado modulo 'hr_timeshee_project_task_stage'.

--- project_task_calendar/models/account_analytic_line.py
+++ project_task_calendar/models/account_analytic_line.py
@@ -9,8 +9,8 @@ from odoo import fields, models
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
-    project_task_type_id = fields.Many2one('project.task.type',
-                                           string='Project Task State')
+    # project_task_type_id = fields.Many2one('project.task.type',
+    #                                        string='Project Task State')
 
     calendar_event_id = fields.Many2one(comodel_name='calendar.event',
                                         readonly=True,
