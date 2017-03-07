e6b77ea705843765de0e1685e211c1103edd606d
Michell Stuttgart <michellstut@gmail.com>
2017-03-07 17:26:03 -0300

    [NEW] Adicionado modulo 'project_partner_menu_in_project' e 'project_calendar_menu_in_project'

---
+++ project_calendar_menu_in_project/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Calendar Menu in Project',
+    'version': '10.0.1.0.0',
+    'author': 'MultidadosTI',
+    'maintainer': 'MultidadosTI',
+    'website': 'www.multidadosti.com.br',
+    'license': 'LGPL-3',
+    'category': 'Project',
+    'summary': 'This module add calendar menu in project',
+    'contributors': [
+        'Michell Stuttgart <michellstut@gmail.com>',
+        'Rodrigo Ferreira <rodrigo.santos@multidadosti.com.br>',
+    ],
+    'depends': [
+        'project',
+        'calendar',
+    ],
+    'data': [
+        'views/calendar_event.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ project_partner_menu_in_project/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Partner Menu in Project',
+    'version': '10.0.1.0.0',
+    'author': 'MultidadosTI',
+    'maintainer': 'MultidadosTI',
+    'website': 'www.multidadosti.com.br',
+    'license': 'LGPL-3',
+    'category': 'Project',
+    'summary': 'This module add partner menu in project',
+    'contributors': [
+        'Aldo Soares <soares_aldo@hotmail.com>',
+        'Michell Stuttgart <michellstut@gmail.com>',
+    ],
+    'depends': [
+        'project',
+    ],
+    'data': [
+            'views/res_partner.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
