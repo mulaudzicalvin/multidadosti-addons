a7d76e83f32ae9c8f2e98cea7765bd473abe85a9
Michell Stuttgart <michellstut@gmail.com>
2017-03-09 17:22:53 -0300

    [NEW] Adicionado modulo 'calendar_event_partner'

---
+++ calendar_event_partner/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Calendar Event Partner',
+    'version': '10.0.1.0.0',
+    'author': 'MultidadosTI',
+    'maintainer': 'MultidadosTI',
+    'website': 'www.multidadosti.com.br',
+    'license': 'LGPL-3',
+    'category': 'Extra Tools',
+    'summary': 'Add relation with partner in calendar event',
+    'contributors': [
+        'Michell Stuttgart <michellstut@gmail.com>',
+        'Rodrigo Ferreira <rodrigo.santos@multidadosti.com.br>',
+    ],
+    'depends': [
+        'calendar',
+    ],
+    'data': [
+        'views/calendar_event.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
