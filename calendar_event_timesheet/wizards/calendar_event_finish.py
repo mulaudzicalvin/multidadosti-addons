4aec5cf0bd09f9ce33062798d887df87d06a9f67
Michell Stuttgart <michellstut@gmail.com>
2017-03-10 16:52:15 -0300

    [FIX] Ajuste no metodo de criação de timesheet a partir do evento de calendario

--- calendar_event_timesheet/wizards/calendar_event_finish.py
+++ calendar_event_timesheet/wizards/calendar_event_finish.py
@@ -25,21 +25,30 @@ class CalendarEventFinish(models.TransientModel):
             users = \
                 self.env['res.users'].search([('partner_id', 'in', partners)])
 
+            # Create Timesheet to any user in users list
             for user in users:
-
-                values = {
-                    'name': ce.event_feedback,
-                    'date': dt.date(),
-                    'user_id': user.id,
-                    'project_id': ce.project_id.id,
-                    'unit_amount': ce.event_duration,
-                    'calendar_event_id': ce.id,
-                }
-
-                if ce.task_id:
-                    values['task_id'] = ce.task_id.id
-                    values['project_task_type_id'] = ce.task_id.stage_id.id
+                values = self._get_account_analytic_line_values(
+                    user=user, calendar_event=ce, start_datetime=dt)
 
                 self.env['account.analytic.line'].create(values)
 
         return res
+
+    @api.multi
+    def _get_account_analytic_line_values(self, user, calendar_event,
+                                          start_datetime):
+        self.ensure_one()
+        values = {
+            'name': calendar_event.event_feedback,
+            'date': start_datetime.date(),
+            'user_id': user.id,
+            'project_id': calendar_event.project_id.id,
+            'unit_amount': calendar_event.event_duration,
+            'calendar_event_id': calendar_event.id,
+        }
+
+        if calendar_event.task_id:
+            values['task_id'] = calendar_event.task_id.id
+            values['project_task_type_id'] = calendar_event.task_id.stage_id.id
+
+        return values
