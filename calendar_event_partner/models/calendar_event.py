a3847c345f870c9a852948f000021e9e8a0db8ad
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2017-03-21 18:45:45 -0300

    [FIX]Adicionado campo partner ao calendario kanban no modulo 'calendat_event_partner'

--- calendar_event_partner/models/calendar_event.py
+++ calendar_event_partner/models/calendar_event.py
@@ -19,3 +19,10 @@ class CalendarEvent(models.Model):
 
     company_partner_id = fields.Many2one(comodel_name='res.partner',
                                          default=get_company_partner)
+
+    partner_name = fields.Char(compute='_compute_record_name')
+
+    @api.depends('partner_id')
+    def _compute_record_name(self):
+        for rec in self:
+            rec.partner_name = ' * ' + rec.partner_id.name if rec.partner_id else ''
