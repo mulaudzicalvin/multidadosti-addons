7897a66510bc72b151e3259ccb205fac43688dd3
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2017-03-22 11:22:18 -0300

    [NEW]Módulo criado 'base_web_base_url_freeze' para fixar o ip do servidor

---
+++ base_web_base_url_freeze/README.md
@@ -0,0 +1,15 @@
+### Web Base Url Freeze
+
+This feature adds a system parameter to fix the server url.
+
+### Bug Tracker
+
+Bugs are tracked on [GitHub Issues](https://github.com/multidadosti-erp/multidadosti-addons/issues).
+
+In case of trouble, please check there if your issue has already been reported.
+
+### Contributors
+
+* Aldo Soares <soares_aldo@hotmail.com>
+* Michell Stuttgart <michellstut@gmail.com>
+* Rodrigo Ferreira <rodrigosferreira91@gmail.com>
---
+++ base_web_base_url_freeze/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Web Base Url Freeze',
+    'version': '10.0.1.0.0',
+    'author': 'MultidadosTI',
+    'maintainer': 'MultidadosTI',
+    'website': 'www.multidadosti.com.br',
+    'license': 'LGPL-3',
+    'category': 'Others',
+    'summary': """Module to create web.base.url.freeze parameter.""",
+    'contributors': [
+        'Rodrigo Ferreira <rodrigosferreira91@gmail.com>',
+    ],
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'data/ir_config_parameter.xml'
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
