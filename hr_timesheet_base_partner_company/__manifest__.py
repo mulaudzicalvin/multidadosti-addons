a1851cf20d1b55027f79d8f14b873719ccaf1c25
Aldo_Soares <soares_aldo@hotmail.com>
2017-03-27 11:58:26 -0300

    [NEW] Criado módulo que exibe campos partner_id e company_id na tree view de account_analytic_line

---
+++ hr_timesheet_base_partner_company/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'HR Timesheet - Base Partner and Company',
+    'license': 'AGPL-3',
+    'author': 'MultidadosTI',
+    'version': '10.0.1.0.0',
+    'website': 'www.multidadosti.com.br',
+    'summary': """This module adds the collumns partner_id and
+            company_id in tree view of module account_analytic_line.""",
+    'category': 'Timesheets',
+    'contributors': [
+        'Aldo Soares <soares_aldo@hotmail.com>',
+    ],
+    'depends': [
+        'hr_timesheet',
+    ],
+    'data': [
+        'views/account_analytic_line.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
