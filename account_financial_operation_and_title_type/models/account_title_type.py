841414ba287c8bf619c6ad7cfbe4d71ff9594ed6
Aldo_Soares <soares_aldo@hotmail.com>
2017-03-28 18:01:20 -0300

    [REF] Alteração em campo minimum_plot_value da model account.title.type

--- account_financial_operation_and_title_type/models/account_title_type.py
+++ account_financial_operation_and_title_type/models/account_title_type.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models
+import odoo.addons.decimal_precision as dp
 
 
 class AccountTitleType(models.Model):
@@ -11,6 +12,7 @@ class AccountTitleType(models.Model):
 
     description = fields.Char(string='Description')
 
-    minimum_plot_value = fields.Float(string='Minimum Plot Value')
+    minimum_plot_value = fields.Float(string='Minimum Plot Value',
+                                      digits=dp.get_precision('Product Price'))
 
     allow_cnab_sending = fields.Boolean(string='Allow CNAB Sending')
