465c8b60a9df1456f0ef6939aeffd2bbebb84029
Aldo_Soares <soares_aldo@hotmail.com>
2017-03-28 14:38:13 -0300

    [NEW] Criação de classe account.title.type

--- account_financial_operation_and_title_type/models/account_title_type.py
+++ account_financial_operation_and_title_type/models/account_title_type.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class AccountTitleType(models.Model):
+
+    _name = 'account.title.type'
+
+    initials = fields.Char(string='Initials', size=3)
+
+    description = fields.Char(string='Description')
+
+    minimum_plot_value = fields.Float(string='Minimum Plot Value')
+
+    allow_cnab_sending = fields.Boolean(string='Allow CNAB Sending')
