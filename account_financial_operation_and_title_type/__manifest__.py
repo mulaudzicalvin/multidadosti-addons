63ce80d81063627de6cfc020327a67eff9facdd9
Aldo_Soares <soares_aldo@hotmail.com>
2017-03-28 14:31:07 -0300

    [NEW] Criação de módulo para adicionar campo na model account.move.line

---
+++ account_financial_operation_and_title_type/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Account - New fields: Financial Operation and Title Type',
+    'version': '10.0.1.0.0',
+    'author': 'MultidadosTI',
+    'maintainer': 'MultidadosTI',
+    'website': 'www.multidadosti.com.br',
+    'license': 'LGPL-3',
+    'category': 'Account',
+    'summary': """This Module adds new fields: Financial Operations and Title
+     Type in account.move.line model""",
+    'contributors': [
+        'Aldo Soares <soares_aldo@hotmail.com>',
+    ],
+    'depends': [
+        'br_account_payment',
+    ],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ account_financial_operation_and_title_type/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import account_financial_operation
+from . import account_title_type
