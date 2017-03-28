0d008537eaee95a02f3d6babfb9b2e41ce4d8162
Aldo_Soares <soares_aldo@hotmail.com>
2017-03-28 14:37:07 -0300

    [NEW] Criação de classe account.financial.operation

--- account_financial_operation_and_title_type/models/account_financial_operation.py
+++ account_financial_operation_and_title_type/models/account_financial_operation.py
@@ -2,3 +2,11 @@
 
 from odoo import fields, models
 
+
+class AccountFinancialOperation(models.Model):
+
+    _name = 'account.financial.operation'
+
+    description = fields.Char(string='Description')
+
+    active = fields.Boolean(string='Active')
