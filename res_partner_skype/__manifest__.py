a8f4c8835fddad0eaf7b809a77ad4468c438feb9
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2017-03-31 11:05:07 -0300

    [FIX]Módulo que adiciona o campo skype na tela de contato no cadastro de clientes

---
+++ res_partner_skype/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Partner Skype',
+    'version': '10.0.1.0.0',
+    'author': 'MultidadosTI',
+    'maintainer': 'MultidadosTI',
+    'website': 'www.multidadosti.com.br',
+    'license': 'LGPL-3',
+    'category': 'Extra tools',
+    'summary': 'Sets the default filter in project task module',
+    'contributors': [
+        'Rodrigo Ferreira <rodrigosferreira91@gmail.com>',
+    ],
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner_skype.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
