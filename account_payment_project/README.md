34408a65380db8836692df305649d4972cd38a0a
Aldo_Soares <soares_aldo@hotmail.com>
2017-04-07 10:27:55 -0300

    [NEW] Criação de módulo que estabelece relação entre account.payment e project.project

---
+++ account_payment_project/README.md
@@ -0,0 +1,13 @@
+## Account Payment - Defines a relation between account.payment and project.project
+
+This module defines a relation between account.payment and project.project
+
+### Bug Tracker
+
+Bugs are tracked on [GitHub Issues](https://github.com/multidadosti-erp/multidadosti-addons/issues).
+
+In case of trouble, please check there if your issue has already been reported.
+
+### Contributors
+
+* Aldo Soares <soares_aldo@hotmail.com>
---
+++ account_payment_project/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Account Payment - Defines a relation between account.payment and '
+            'project.project',
+    'version': '10.0.1.0.0',
+    'author': 'MultidadosTI',
+    'maintainer': 'MultidadosTI',
+    'website': 'www.multidadosti.com.br',
+    'license': 'LGPL-3',
+    'category': 'Account',
+    'summary': """This module defines a relation between account.payment and
+                project.project""",
+    'contributors': [
+        'Aldo Soares <soares_aldo@hotmail.com>',
+    ],
+    'depends': [
+        'general_payments',
+    ],
+    'data': [
+        'views/account_payment.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
