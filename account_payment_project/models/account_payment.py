63fb7adc2471da47ed901df77e6ff45de3841979
Aldo_Soares <soares_aldo@hotmail.com>
2017-04-07 10:29:15 -0300

    [IMP] Inclusão do campo project_id a model account.payment

---
+++ account_payment_project/models/account_payment.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    project_id = fields.Many2one(string='Project',
+                                 comodel_name='project.project')
