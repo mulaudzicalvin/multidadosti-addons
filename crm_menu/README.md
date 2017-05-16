ab385205bc9bae39c0b354389fac0ebf4de0c4a4
rodrigets <rodrigosferreira91@gmail.com>
2017-05-16 14:31:05 -0300

    [NEW]Novo módulo que cria o menu CRM, com painel, configurações e menu crm disponíveis

---
+++ crm_menu/README.md
@@ -0,0 +1,13 @@
+## CRM Menu
+
+This module creates the CRM menu.
+
+### Bug Tracker
+
+Bugs are tracked on [GitHub Issues](https://github.com/multidadosti-erp/multidadosti-addons/issues).
+
+In case of trouble, please check there if your issue has already been reported.
+
+### Contributors
+
+* Rodrigo Ferreira <rodrigosferreira91@gmail.com>
---
+++ crm_menu/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'CRM Menu',
+    'license': 'LGPL-3',
+    'author': 'MultidadosTI',
+    'version': '10.0.1.0.0',
+    'website': 'www.multidadosti.com.br',
+    'summary': 'This module creates the CRM menu',
+    'category': 'Web',
+    'contributors': [
+        'Rodrigo Ferreira <rodrigosferreira91@gmail.com>',
+    ],
+    'depends': [
+        'crm',
+    ],
+    'data': [
+        'views/crm_menu.xml'
+
+    ],
+    'installable': True,
+}
