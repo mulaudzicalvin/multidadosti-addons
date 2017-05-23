e646bb94faa7a81056363b054fdf2f049dd931da
rodrigets <rodrigosferreira91@gmail.com>
2017-05-23 11:54:14 -0300

    [FIX]No cadastro de cliente deixei o campo 'parent_id' invisible e o campo 'company_type' default 'company'

--- base_partner_is_not_customer/models/res_partner.py
+++ base_partner_is_not_customer/models/res_partner.py
@@ -8,4 +8,5 @@ class Partner(models.Model):
     _inherit = "res.partner"
 
     is_company = fields.Boolean(string='Is a Company', default=True,
-                            help="Check if the contact is a company, otherwise it is a person")
+                                help="Check if the contact is a company, "
+                                     "otherwise it is a person")
