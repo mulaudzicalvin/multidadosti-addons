29d6a1cf6ffc0ac36aa03740a02d30c87da890b4
rodrigets <rodrigosferreira91@gmail.com>
2017-05-23 11:33:15 -0300

    [FIX]No cadastro de cliente deixei o campo 'parent_id' invisible e o campo 'company_type' default 'company'

---
+++ base_partner_is_not_customer/models/res_partner.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import fields, models
+
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+
+    is_company = fields.Boolean(string='Is a Company', default=True,
+                            help="Check if the contact is a company, otherwise it is a person")
