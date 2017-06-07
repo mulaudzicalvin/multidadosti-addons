4b9617d50c9eb4da973140f23128006d5c0f759c
Michell Stuttgart <michellstut@gmail.com>
2017-06-07 14:35:36 -0300

    [REF] Campo renomeando para 'attachment_category_id'

--- project_attachment_tags/models/ir_attachment.py
+++ project_attachment_tags/models/ir_attachment.py
@@ -17,7 +17,7 @@ class IrAttachment(models.Model):
     def _onchange_category(self):
         if self.res_model == 'project.project':
             self.category_id = self.env['project.project'].browse(
-                self.res_id).attachment_category
+                self.res_id).attachment_category_id
 
         lista_ids = self.category_id.line_ids.ids if self.category_id else []
         domain = {'domain': {'tag_id': [('id', '=', lista_ids)]}}
--- project_attachment_tags/models/project_project.py
+++ project_attachment_tags/models/project_project.py
@@ -7,5 +7,5 @@ class ProjectProject(models.Model):
 
     _inherit = "project.project"
 
-    attachment_category = fields.Many2one(string='Tags',
-                                          comodel_name='attach.category')
+    attachment_category_id = fields.Many2one(string='Tags',
+                                             comodel_name='attach.category')
