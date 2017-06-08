249aff36564e7fa5ee662aab8b6883d76074693f
Michell Stuttgart <michellstut@gmail.com>
2017-06-08 11:24:39 -0300

    [REF] E necessario apenas a sobrescrita do atributo 'default'

--- base_partner_is_not_customer/models/res_partner.py
+++ base_partner_is_not_customer/models/res_partner.py
@@ -7,6 +7,4 @@ from odoo import fields, models
 class Partner(models.Model):
     _inherit = "res.partner"
 
-    is_company = fields.Boolean(string='Is a Company', default=True,
-                                help="Check if the contact is a company, "
-                                     "otherwise it is a person")
+    is_company = fields.Boolean(default=True)
