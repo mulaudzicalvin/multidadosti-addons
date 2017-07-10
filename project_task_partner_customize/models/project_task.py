0516ed815bfb034359a9937a5c4790810f6d7473
Michell Stuttgart <michellstut@gmail.com>
2017-07-10 09:35:42 -0300

    Update project_task.py

--- project_task_partner_customize/models/project_task.py
+++ project_task_partner_customize/models/project_task.py
@@ -13,9 +13,10 @@ class ProjectTask(models.Model):
     @api.constrains('user_id')
     def _check_task(self):
         msg = 'Tarefa: ' + self.name + u' atribuída a você.'
-        post_vars = {'subject': 'Notification',
-                     'body':  msg,
-                     'partner_ids': [(4, self.user_id.partner_id.id)],
-                     }
+        post_vars = {
+            'subject': 'Notification',
+            'body':  msg,
+            'partner_ids': [(4, self.user_id.partner_id.id)],
+        }
         self.env['mail.thread'].message_post(
             type="notification", subtype="mt_comment", **post_vars)
