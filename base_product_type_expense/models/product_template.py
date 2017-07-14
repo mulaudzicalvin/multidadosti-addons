8b1f3fbfbabb3919b69fbd05a982bff27a859bd6
Michell Stuttgart <michellstut@gmail.com>
2017-07-14 16:04:23 -0300

    [NEW] Adicionado testes unitarios para o modulo 'base_product_type_expense'

--- base_product_type_expense/models/product_template.py
+++ base_product_type_expense/models/product_template.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = "product.template"
+    _inherit = 'product.template'
 
     type = fields.Selection(selection_add=[('expense', 'Expense')])
 
