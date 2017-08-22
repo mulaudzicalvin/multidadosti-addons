bf27e266e16c0f41f4784312236457c38b08fc6b
Michell Stuttgart <michellstut@gmail.com>
2017-08-22 13:48:26 -0300

    [IMP] Adicionado mock para datetime.now() nos testes de 'helpdesk_phonecall_timesheet'

--- helpdesk_phonecall_timesheet/tests/test_helpdesk_phonecall_confirm.py
+++ helpdesk_phonecall_timesheet/tests/test_helpdesk_phonecall_confirm.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
-import datetime
+
+import mock
 
 from odoo.exceptions import UserError
-
-from odoo.addons.helpdesk_phonecall_support.tests.\
-    test_helpdesk_phonecall_confirm import TestHelpDeskPhoneCallConfirm
+from odoo.tools import float_compare
+from odoo.addons.helpdesk_phonecall_support.tests.test_helpdesk_phonecall_confirm import TestHelpDeskPhoneCallConfirm  # noqa: 501
 
 
 class TestHelpDeskPhoneCallConfirmTimesheet(TestHelpDeskPhoneCallConfirm):
-
     def setUp(self):
         super(TestHelpDeskPhoneCallConfirmTimesheet, self).setUp()
 
     def test_action_confirm_finish_phonecall(self):
-
         tag = 'helpdesk_phonecall_support.helpdesk_phonecall_service_tag_01'
+
+        start_date_hour = '2017-09-04 14:49:20'
+        finish_date_hour = '2017-09-04 15:20:40'
 
         values = {
             'partner_id': self.partner.id,
@@ -22,6 +23,7 @@ class TestHelpDeskPhoneCallConfirmTimesheet(TestHelpDeskPhoneCallConfirm):
             'project_id': self.project.id,
             'description': 'Teste',
             'phonecall_tag_id': self.env.ref(tag).id,
+            'start_date_hour': start_date_hour,
         }
 
         # Finalmente criamos um atendimento
@@ -31,17 +33,22 @@ class TestHelpDeskPhoneCallConfirmTimesheet(TestHelpDeskPhoneCallConfirm):
         wizard = self.env['helpdesk.phonecall.confirm'].with_context(
             {'active_ids': [phonecall.id]})
 
-        # Executamos o metodo de finalizar o atendimento
-        wizard.action_confirm_finish_phonecall()
+        # Executamos o metodo de finalizar o atendimento. Utilizamos o mock
+        # para retornamos a data que queremos para que assim seja mais facil
+        # verificar se o calculo de data esta correto
+        with mock.patch('odoo.fields.Datetime.now') as dt:
+            dt.return_value = finish_date_hour
+            wizard.action_confirm_finish_phonecall()
 
-        self.assertNotEqual(phonecall.finish_date_hour, False)
+        self.assertTrue(phonecall.finish_date_hour)
         self.assertEqual(phonecall.state, 'done')
+        self.assertEqual(phonecall.finish_date_hour, finish_date_hour)
 
         # Executamos o metodo de finalizar o atendimento, verificamos
         # se ele obedece o conceito que apenas atendimentos em aberto podem
         # ser finalizados
-        self.assertRaises(wizard.action_confirm_finish_phonecall, None,
-                          UserError)
+        with self.assertRaises(UserError):
+            wizard.action_confirm_finish_phonecall()
 
         timesheet = self.env['account.analytic.line'].search(
             [('helpdesk_id', '=', phonecall.id)])
@@ -53,15 +60,15 @@ class TestHelpDeskPhoneCallConfirmTimesheet(TestHelpDeskPhoneCallConfirm):
         self.assertEqual(phonecall.company_id.id, timesheet.company_id.id)
         self.assertEqual(phonecall.start_date_hour[:10], timesheet.date)
 
-        # Calculamos o tempo de duracao e verificamos se o tempo correto
-        # esta sendo armazenado
-        fmt = '%Y-%m-%d %H:%M:%S'
-        d1 = datetime.datetime.strptime(phonecall.start_date_hour, fmt)
-        d2 = datetime.datetime.strptime(phonecall.finish_date_hour, fmt)
+        # Verificamos a diferença de datas em segundos e a convertemos para
+        # horas, já que o widget 'float_time' do Odoo considera o valor de
+        # horas em segundos
+        hours_diff_decimal = 1880 / 3600.0
 
-        # Convert total seconds in hours in decimal format
-        # When load the float field in screen, the widget
-        # 'float_time' will go show time in to HH:MM format
-        hours_diff_decimal = (d2 - d1).total_seconds() / 3600.0
+        result = float_compare(timesheet.unit_amount,
+                               hours_diff_decimal,
+                               precision_digits=12)
 
-        self.assertEqual(timesheet.unit_amount, hours_diff_decimal)
+        # Verificamos se o resultado da comparacao e zero. No Python o valor
+        # e considerado False
+        self.assertFalse(result)
