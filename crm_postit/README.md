35bcf76f060836ccbe7abeb1b357a1ad123570a1
Michell Stuttgart <michellstut@gmail.com>
2017-08-23 16:14:09 -0300

    [NEW] Adicionado modulo 'crm_postit'

---
+++ crm_postit/README.md
@@ -0,0 +1,8 @@
+## CRM Postit
+
+This module creates a relation between CRM and PostIT.
+
+### Contribuidores
+
+* Michell Stuttgart <michellstut@gmail.com>
+* Rodrigo Ferreira <rodrigosferreira91@gmail.com>
---
+++ crm_postit/models/postit.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from odoo import api, fields, models
+
+
+class PrismePostIt(models.Model):
+    """ Post It data """
+    _inherit = 'prisme.postit'
+
+    opportunity_count = fields.Integer(string='Opportunity',
+                                       compute='_compute_opportunity_count')
+
+    lead_ids = fields.Many2many(comodel_name='crm.lead', string='Leads')
+
+    @api.multi
+    def _compute_opportunity_count(self):
+        for postit in self:
+            postit.opportunity_count = self.env['crm.lead'].search_count(
+                [('postit_ids', '=', postit.id)])
+
+    @api.multi
+    def action_redirect_crm_lead(self):
+        action = self.env['ir.actions.act_window'].for_xml_id(
+            'crm', 'crm_lead_opportunities')
+
+        action['domain'] = [('postit_ids', '=', self.id)]
+        action['context'] = {
+            'default_postit_ids': self.ids,
+            'default_partner_id': self.partner_id.id,
+        }
+        return action
