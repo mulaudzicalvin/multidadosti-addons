bb3ed6a4ce4fb2ca8b8c21e5c0a6f551dbc0a78f
Michell Stuttgart <michellstut@gmail.com>
2017-08-31 14:42:06 -0300

    [FIX] Erro ao criar tarefa sem atribuir a usuario

--- project_task_partner_customize/models/project_task.py
+++ project_task_partner_customize/models/project_task.py
@@ -10,13 +10,33 @@ class ProjectTask(models.Model):
                                    related='project_id.partner_ids',
                                    string='Related Partners')
 
-    @api.constrains('user_id')
-    def _check_task(self):
-        msg = u'Tarefa: %s atribuída a você.' % self.name
+    # Atributo default foi sobrescrito para evitar que o usuario fosse
+    # atribuido automaticamente por engano
+    user_id = fields.Many2one(default=False)
+
+    @api.model
+    def create(self, values):
+        project_task = super(ProjectTask, self).create(values)
+
+        if 'user_id' in values and values.get('user_id'):
+            self._send_message()
+
+        return project_task
+
+    @api.multi
+    def write(self, values):
+        ret = super(ProjectTask, self).write(values)
+
+        if 'user_id' in values and values.get('user_id'):
+            self._send_message()
+
+        return ret
+
+    def _send_message(self):
 
         post_vars = {
             'subject': u'Nova tarefa atribuída a você',
-            'body':  msg,
+            'body': u'Tarefa: %s atribuída a você.' % self.name,
             'partner_ids': [(4, self.user_id.partner_id.id)],
             'message_type': 'notification',
             'subtype': 'mt_comment',
