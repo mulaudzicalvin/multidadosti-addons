7503b7bf34382e810930c484e78a3665f95f9bf0
Michell Stuttgart <michellstut@gmail.com>
2017-09-08 17:26:36 -0300

    [REF] Formatacao de codigo em 'project_quality_classification'

--- project_quality_classification/models/project_task.py
+++ project_quality_classification/models/project_task.py
@@ -2,16 +2,16 @@
 
 from odoo import fields, models
 
+PRIORITY_CLASSIFICATION = [
+    ('0', 'Lowest'),
+    ('1', 'Low'),
+    ('2', 'Medium'),
+    ('3', 'Hight'),
+    ('4', 'Highest'),
+]
+
 
 class ProjectTask(models.Model):
     _inherit = 'project.task'
-
-    PRIORITY_CLASSIFICATION = [
-        ('0', 'Lowest'),
-        ('1', 'Low'),
-        ('2', 'Medium'),
-        ('3', 'Hight'),
-        ('4', 'Highest'),
-    ]
 
     priority = fields.Selection(selection=PRIORITY_CLASSIFICATION)
