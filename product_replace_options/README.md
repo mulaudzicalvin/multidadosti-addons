7c142c72bec68cb9397f55c499751df06a32019d
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2017-09-26 14:30:19 -0300

    [NEW] Módulo para esconder opções não utilizadas no cadastro de produto

---
+++ product_replace_options/README.md
@@ -0,0 +1,13 @@
+## Product Replace - Options
+
+This module customizes the options for the product registration.
+
+### Bug Tracker
+
+Bugs are tracked on [GitHub Issues](https://github.com/multidadosti-erp/multidadosti-addons/issues).
+
+In case of trouble, please check there if your issue has already been reported.
+
+### Contributors
+
+* Rodrigo Ferreira <rodrigosferreira91@gmail.com>
---
+++ product_replace_options/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Replace',
+    'license': 'LGPL-3',
+    'author': 'MultidadosTI',
+    'version': '10.0.1.0.0',
+    'website': 'www.multidadosti.com.br',
+    'summary': 'This module customizes the options for the product '
+               'registration',
+    'category': 'Product',
+    'contributors': [
+        'Rodrigo Ferreira <rodrigosferreira91@gmail.com>',
+    ],
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
