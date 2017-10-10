e5aa6d79d353d3677f62d96fa55eb44cebd5a876
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2017-10-10 16:56:40 -0300

    [REF] Adicionado test para os metodos e remoção do campo attachment e dos metodos ligados, ira ser criado outro modulo para esta ação

---
+++ crm_lead_is_customer/tests/test_crm_lead.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo.tests.common import TransactionCase
+
+
+class TestCrmLeadIsCustomer(TransactionCase):
+
+    def setUp(self):
+        super(TestCrmLeadIsCustomer, self).setUp()
+
+        self.crm_lead = self.env.ref('crm.crm_case_26')
+
+    def test_action_set_won(self):
+        self.crm_lead.action_set_won()
+        self.assertTrue(self.crm_lead.partner_id.customer)
+        self.assertFalse(self.crm_lead.next_activity_id)
+
+        self.crm_lead.partner_id = False
+        self.crm_lead.action_set_won()
+        self.assertFalse(self.crm_lead.partner_id.customer)
+        self.assertFalse(self.crm_lead.next_activity_id)
+
+    def test_action_set_lost(self):
+        self.crm_lead.action_set_lost()
+        self.assertFalse(self.crm_lead.next_activity_id)
