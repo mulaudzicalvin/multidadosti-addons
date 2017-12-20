f07d2903fb9d3b94da402474f21b04e52ac7851f
Aldo_Soares <soares_aldo@hotmail.com>
2017-12-20 15:56:26 -0200

    [FIX] Ajuste em campo de categorização de títulos

--- general_payments/models/account_move.py
+++ general_payments/models/account_move.py
@@ -15,9 +15,11 @@ class AccountMoveLine(models.Model):
     @api.depends('user_type_id', 'date_maturity')
     def _compute_title_status(self):
         for aml in self:
-            if aml.user_type_id.type == 'receivable' and aml.reconciled:
+            if (aml.user_type_id.type == 'receivable' and aml.reconciled and
+                    aml.payment_datetime):
                 aml.title_status = 'received'
-            elif aml.user_type_id.type == 'payable' and aml.reconciled:
+            elif (aml.user_type_id.type == 'payable' and aml.reconciled and
+                    aml.payment_datetime):
                 aml.title_status = 'payed'
             elif (not aml.reconciled and aml.date_maturity >
                     datetime.strftime(
