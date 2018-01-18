ae69e0d35e43054827b25d7ac632fe02a819577e
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2018-01-18 11:15:36 -0200

    [REF] Inclusão de campo compute para sincronizar foto do parceiro e usuario.

---
+++ base_user_from_partner/models/res_partner.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class Partner(models.Model):
+
+    _inherit = "res.partner"
+
+    user_id = fields.Many2one('res.users', compute='_compute_user_id')
+
+    @api.multi
+    def _compute_user_id(self):
+        for rec in self:
+            user = self.env['res.users'].search([('partner_id', '=', rec.id)])
+            if user:
+                rec.user_id = user.id
+                user.image = rec.image
