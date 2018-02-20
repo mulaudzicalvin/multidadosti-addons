6d4f32bc73b1acaaf9fd9bdd1d040cb6aaff4965
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2018-02-20 16:04:05 -0300

    [MIG] project_planned_time

--- project_planned_time/__manifest__.py
+++ project_planned_time/__manifest__.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-
 {
     'name': 'Project Planned Time',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'author': 'MultidadosTI',
     'maintainer': 'MultidadosTI',
     'website': 'www.multidadosti.com.br',
@@ -20,7 +18,7 @@
     'data': [
         'views/project_project.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
