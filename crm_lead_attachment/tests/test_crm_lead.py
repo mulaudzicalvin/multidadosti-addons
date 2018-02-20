e41cd347543647d960f2ca6be91b60c1c67ebc9b
Michell Stuttgart <michellstut@gmail.com>
2018-02-20 17:50:11 -0300

    [MIG] crm_lead_attachment: Migration to 11.0

--- crm_lead_attachment/tests/test_crm_lead.py
+++ crm_lead_attachment/tests/test_crm_lead.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
 from odoo.tests.common import TransactionCase
 
 
 class TestCrmLeadAttachment(TransactionCase):
+    
     def setUp(self):
         super(TestCrmLeadAttachment, self).setUp()
 
