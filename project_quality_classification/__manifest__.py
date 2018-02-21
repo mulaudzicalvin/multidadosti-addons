31830637a82fc0a398c8c85abe23c63e830202cd
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2018-02-21 17:13:15 -0300

    [MIG] project_quality_classification

--- project_quality_classification/__manifest__.py
+++ project_quality_classification/__manifest__.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-
 {
     'name': 'Project Quality Classification',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'author': 'MultidadosTI',
     'maintainer': 'MultidadosTI',
     'website': 'www.multidadosti.com.br',
@@ -21,7 +19,7 @@
         'views/project_project.xml',
     ],
     'uninstall_hook': 'uninstall_hook',
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
--- project_quality_classification/models/__init__.py
+++ project_quality_classification/models/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 from . import project_project
 from . import project_task
