175353696b5b371386a6adb91a6ff352a626a85d
Aldo_Soares <soares_aldo@hotmail.com>
2018-02-23 17:46:12 -0300

    [MIG] Migração do módulo hr_timesheet_to_invoice da versão 10.0 para 11.0

--- hr_timesheet_to_invoice/__manifest__.py
+++ hr_timesheet_to_invoice/__manifest__.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-
 {
     'name': 'HR Timesheet to Invoice',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'author': 'MultidadosTI',
     'maintainer': 'MultidadosTI',
     'website': 'www.multidadosti.com.br',
@@ -22,7 +20,7 @@
         'views/account_analytic_line_view.xml',
         'views/account_analytic_line_to_invoice.xml'
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
--- hr_timesheet_to_invoice/models/account_analytic_line.py
+++ hr_timesheet_to_invoice/models/account_analytic_line.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
-
 from odoo import fields, models
 
 
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
-    account_analytic_line_to_invoice_id = \
-        fields.Many2one(comodel_name='account.analytic.line.to.invoice',
-                        string='To Invoice')
+    account_analytic_line_to_invoice_id = fields.Many2one(
+        comodel_name='account.analytic.line.to.invoice',
+        string='To Invoice')
