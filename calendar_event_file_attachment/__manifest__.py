db2c48be8c17c7a929978b933cc32271d6fa1549
Michell Stuttgart <michellstut@gmail.com>
2018-02-26 13:46:58 -0300

    [MIG] calendar_event_file_attachment: Migration to 11.0

--- calendar_event_file_attachment/__manifest__.py
+++ calendar_event_file_attachment/__manifest__.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-
 {
     'name': 'Calendar Event File Attachment',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'author': 'MultidadosTI',
     'maintainer': 'MultidadosTI',
     'website': 'www.multidadosti.com.br',
@@ -18,7 +16,7 @@
     'data': [
         'views/calendar_event.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
--- calendar_event_file_attachment/models/__init__.py
+++ calendar_event_file_attachment/models/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 from . import calendar_event
 from . import project_project
