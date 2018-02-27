893949111f132e952fc5e028c4a86ca626209844
Aldo_Soares <soares_aldo@hotmail.com>
2018-02-27 16:09:56 -0300

    [MIG] account_replace_invoice_menu_sale: Migration to 11.0

--- account_replace_invoice_menu_sale/__manifest__.py
+++ account_replace_invoice_menu_sale/__manifest__.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-
 {
     'name': 'Account Replace Invoice Menu in Sale',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'author': 'MultidadosTI',
     'maintainer': 'MultidadosTI',
     'website': 'www.multidadosti.com.br',
@@ -23,7 +21,7 @@
         'views/sale.xml',
         'views/crm_team_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
