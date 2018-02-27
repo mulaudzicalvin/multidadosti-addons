d5c57513be5050fdde69717cb037608926ff84ca
Aldo_Soares <soares_aldo@hotmail.com>
2018-02-27 16:05:36 -0300

    [MIG] account_replace_invoice_menu: Migration to 11.0

--- account_replace_invoice_menu/__manifest__.py
+++ account_replace_invoice_menu/__manifest__.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-
 {
     'name': 'Account Replace Invoice Menu',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'author': 'MultidadosTI',
     'maintainer': 'MultidadosTI',
     'website': 'www.multidadosti.com.br',
@@ -18,7 +16,7 @@
     'data': [
         'views/account.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
