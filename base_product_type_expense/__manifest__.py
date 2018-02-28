a3405729b69eff9ccd510517a8ba6377376d9799
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2018-02-28 15:41:31 -0300

    [MIG] base_product_type_expense

--- base_product_type_expense/__manifest__.py
+++ base_product_type_expense/__manifest__.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
-
 {
     'name': 'Base Product Type Expense',
     'license': 'LGPL-3',
     'author': 'MultidadosTI',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'website': 'www.multidadosti.com.br',
     'summary': 'Expense registration to use in contracts.',
     'category': 'Base',
@@ -19,7 +17,7 @@
     'data': [
         'views/product_template.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
