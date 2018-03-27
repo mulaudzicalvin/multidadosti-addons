da45a343a78df30f12111992546d79eb667b0bbc
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2018-03-27 16:12:21 -0300

    [MIG] project_task_origin: Migration to 11.0

---
+++ project_task_origin/tests/test_project_project.py
@@ -0,0 +1,34 @@
+from odoo.tests.common import TransactionCase
+
+
+class TestProjectProject(TransactionCase):
+
+    def setUp(self):
+        super(TestProjectProject, self).setUp()
+
+        self.project = self.env['project.project'].create({
+            'name': 'Project Task Origin Test'
+        })
+
+        self.tasks = self.env['project.task'].create({
+            'name': '1',
+            'project_id': self.project.id,
+            'origin': 'feature',
+        })
+
+        self.tasks |= self.env['project.task'].create({
+            'name': '2',
+            'project_id': self.project.id,
+            'origin': 'feature'
+        })
+
+        self.tasks |= self.env['project.task'].create({
+            'name': '3',
+            'project_id': self.project.id,
+            'origin': 'issue'
+        })
+
+    def test__compute_task_origin_count(self):
+        self.project._compute_task_origin_count()
+        self.assertEqual(self.project.task_origin_feature_count, 2)
+        self.assertEqual(self.project.task_origin_issue_count, 1)
